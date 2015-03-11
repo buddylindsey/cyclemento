f951b8e40bea70db3d480933c9a99a678d221853
Buddy Lindsey <blindsey@consumeraffairs.com>
2015-03-10 19:42:10 -0500

    add email logging

--- mento/activities/tasks.py
+++ mento/activities/tasks.py
@@ -11,6 +11,13 @@ from .models import Activity
 from notifications.tasks import check_for_needed_maintenance
 
 
+def log_email(task, message):
+    email = EmailMessage(
+        "[CycleMento] Task: {}".format(task), message,
+        'no-reply@cyclemento.com', ['buddy@buddylindsey.com'])
+    email.send()
+
+
 @app.task
 def get_strava_activities_by_user(user_id, days=None):
     user = User.objects.get(id=user_id)
@@ -25,14 +32,20 @@ def get_strava_activities_by_user(user_id, days=None):
     else:
          activity_iter = client.get_activities()
 
+    acts = []
+
     for activity in activity_iter:
         act, created = Activity.objects.get_or_create(
             external_id=activity.id, source=Activity.SOURCES.strava,
             user=user)
 
         act.update_with_strava(activity)
+        acts.append(act)
 
     check_for_needed_maintenance.delay(user_id)
+
+    log_email('get_strava_activities_by_user',
+              "Total {} added for user {}".format(len(acts), user.username))
 
 
 @app.task
